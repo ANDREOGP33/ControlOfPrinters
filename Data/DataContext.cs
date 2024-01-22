8060467bea35da0799957eae3b5eb1edcb57720e
ANDREOGP33 <andregasparoni690@gmail.com>
2024-01-22 13:43:38 -0300

    Adicionar arquivos de projeto.

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using ControlOfPrinters.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControlOfPrinters.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Printer> printers { get; set; }
+    }
+}
---
+++ Entities/Printer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControlOfPrinters.Entities
+{
+    public class Printer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo IP é obrigatório.")]
+        public string Ip { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O campo Setor é obrigatório.")]
+        public string Sector { get; set; } = string.Empty;
+        public string Level { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O campo Serial é obrigatório.")]
+        public string Serial { get; set; } = string.Empty;
+    }
+}
